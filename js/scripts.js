89bddce14ad8b3ba30c4d640ddd3a24947c35e1b
Austin <Harrenaustint@gmail.com>
2022-04-21 09:46:46 -0400

    pushing portfolio

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,23 @@
 */
 //
 // Scripts
-// 
+//
+const panels = document.querySelectorAll('.panel');
+            
+function toggleOpen() {
+  console.log('Hello');
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive)); 
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -112,3 +128,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove' , (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown' , () => mousedown = true);
 progress.addEventListener('mouseup' , () => mousedown = false);
+
+
